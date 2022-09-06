f0f33cc17a0ff9c72214146ff7ca2218be887ae8
radhavkrishna1994 <radhavkrishna1994@gmail.com>
2022-09-06 18:23:41 +0530

    updated

--- spring-boot-security-demo-db/src/main/java/com/training/config/SecurityConfiguration.java
+++ spring-boot-security-demo-db/src/main/java/com/training/config/SecurityConfiguration.java
@@ -2,13 +2,20 @@ package com.training.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.training.services.MyUserDetailsService;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private MyUserDetailsService userDetailsService;
 	
 	Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
 	
@@ -16,10 +23,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	public void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 		log.info("I am in Authentication");
+		auth
+		.userDetailsService(userDetailsService)
+		.passwordEncoder(getPasswordEncoder());
 			
-		
 	}
 	
+	public  PasswordEncoder getPasswordEncoder() {
+		
+		PasswordEncoder encoder=new PasswordEncoder()
+				{
+
+					@Override
+					public String encode(CharSequence rawPassword) {
+					
+						return rawPassword.toString();
+					}
+
+					@Override
+					public boolean matches(CharSequence rawPassword, String encodedPassword) {
+						
+						return rawPassword.equals(encodedPassword);
+					}
+					
+				};
+		return encoder;
+	}
+
 	//authorization
 	public void configure(HttpSecurity http) throws Exception
 	{
--- spring-boot-security-demo-db/src/main/java/com/training/services/MyUserDetailsService.java
+++ spring-boot-security-demo-db/src/main/java/com/training/services/MyUserDetailsService.java
@@ -1,13 +1,45 @@
 package com.training.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.training.model.MyUser;
+import com.training.repo.UserRepo;
+
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserRepo userRepo;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<MyUser> userOp = userRepo.findById(username);
+		MyUser userFound=null;
+		
+		List<SimpleGrantedAuthority> list=new ArrayList<>();
+		
+		
+		if(userOp.isPresent())
+		{
+			userFound=userOp.get();
+			String roleString = userFound.getRole();
+			for(String role:roleString.split(","))
+			{
+				list.add(new SimpleGrantedAuthority(role));
+			}
+			return new User(username,userFound.getPassword(),list);
+		}
 		
 		return null;
 	}
